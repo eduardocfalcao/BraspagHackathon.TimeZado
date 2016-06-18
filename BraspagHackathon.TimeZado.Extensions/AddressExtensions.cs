7c94bea556f91db9dbad2279125862a973fe7d2c
ivelten <ivelten@braspag.com.br>
2016-06-18 17:19:08 -0300

    Teste commit.

--- BraspagHackathon.TimeZado.Extensions/AddressExtensions.cs
+++ BraspagHackathon.TimeZado.Extensions/AddressExtensions.cs
@@ -10,7 +10,9 @@ namespace BraspagHackathon.TimeZado.Extensions
     {
         public static double DistanceInMilesFrom(this Address remoteAddress, Address localAddress)
         {
-            return (3959 * Math.Acos(Math.Cos(localAddress.Latitude.ToRadians()) * Math.Cos(remoteAddress.Latitude.ToRadians()) * Math.Cos(remoteAddress.Longitude.ToRadians() - localAddress.Longitude.ToRadians()) + Math.Sin(localAddress.Latitude.ToRadians()) * Math.Sin(remoteAddress.Latitude.ToRadians())));
+            const int defaultValue = 3959;
+
+            return (defaultValue * Math.Acos(Math.Cos(localAddress.Latitude.ToRadians()) * Math.Cos(remoteAddress.Latitude.ToRadians()) * Math.Cos(remoteAddress.Longitude.ToRadians() - localAddress.Longitude.ToRadians()) + Math.Sin(localAddress.Latitude.ToRadians()) * Math.Sin(remoteAddress.Latitude.ToRadians())));
         }
     }
 }
