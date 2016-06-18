da22c4ec04bfda20fd8a8438fcb260280a5e633c
lfalcao <eduard.cfalcao@gmail.com>
2016-06-18 17:29:24 -0300

    Subindo cálculo de distância.

--- BraspagHackaton.TimeZado.Services/AddressExtensions.cs
+++ BraspagHackaton.TimeZado.Services/AddressExtensions.cs
@@ -14,5 +14,15 @@ namespace BraspagHackaton.TimeZado.Services
 {
     public static class AddressExtensions
     {
+
+        public static double DistanceInMilesFrom(this Address remoteAddress, Address localAddress)
+        {
+            return (3959 * Math.Acos(Math.Cos(localAddress.Latitude.ToRadians()) 
+                    * Math.Cos(remoteAddress.Latitude.ToRadians()) 
+                    * Math.Cos(remoteAddress.Longitude.ToRadians() 
+                                - localAddress.Longitude.ToRadians()) 
+                                + Math.Sin(localAddress.Latitude.ToRadians()) 
+                                * Math.Sin(remoteAddress.Latitude.ToRadians())));
+        }
     }
 }
